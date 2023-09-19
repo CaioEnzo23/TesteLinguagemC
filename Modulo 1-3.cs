7520f27596ef4083c95a4ae161fb8fe8312f089b
Caio Enzo <138871450+CaioEnzo23@users.noreply.github.com>
2023-09-19 13:08:41 -0300

    Create Modulo 1-3.cs

---
+++ Modulo 1-3.cs
@@ -0,0 +1,47 @@
+Console.WriteLine("Hello, Neos");
+
+Console.Write("Parabéns!");
+Console.Write("");
+Console.Write("Eu escrevi as minhas linhas de código!");
+Console.WriteLine('b');
+Console.WriteLine(12.3m);
+
+string primeiroOla;
+primeiroOla = "Olá, Lillian!";
+string secundaMensagem;
+secundaMensagem = " Você tem 3 mensagens em sua caixa de entrada.";
+string terceiraMensagem;
+terceiraMensagem = "A temperatura é ";
+string numeroDescimal;
+numeroDescimal = "34.4";
+string quartaMensagem;
+quartaMensagem = " graus.";
+Console.Write(primeiroOla);
+Console.Write(secundaMensagem);
+Console.Write(terceiraMensagem);
+Console.Write(numeroDescimal);
+Console.Write(quartaMensagem);
+
+Console.WriteLine("Gerando invoice para consumidor \"ABC Corp\"  ...\n");
+Console.WriteLine("Invoice: 1021\t\tCompleta!");
+Console.WriteLine("Invoice: 1022\t\tCompleta!");
+Console.WriteLine("\nDiretório de saída:\t");
+Console.WriteLine(@"c:\invoices");
+
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F Mundo!");
+
+string primeraPalavra = "Neos";
+string cumprimento = "Olá";
+Console.WriteLine(cumprimento + " " + primeiraPalavra + "!");
+
+string primeiroNome = "Matheus";
+string cumprimento = "Olá";
+Console.WriteLine($"{cumprimento} {primeiroNome}!");
+
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{ensglishLocation}\n");
+
+string russianMessage = "\u841f\u843e\u8441\u843c\u843e\u8442\u0440\u0435\u0442\u044c\u0440\u0444";
+string englishLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMenssage}:\n\t\t{russianLocation}\n");
